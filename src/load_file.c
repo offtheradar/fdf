e76b6af297b3d7ae2e4575a77e512e6f3395c96e
Yacine Sibous <yacinesibous@gmail.com>
2018-03-13 23:48:40 -0700

    add libgfx

--- src/load_file.c
+++ src/load_file.c
@@ -6,7 +6,7 @@
 /*   By: ysibous <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/12 14:38:51 by ysibous           #+#    #+#             */
-/*   Updated: 2018/03/12 16:38:09 by ysibous          ###   ########.fr       */
+/*   Updated: 2018/03/13 16:29:23 by ysibous          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,21 +27,23 @@ int		open_file(char *file_name)
 int		*load_file(char *file_name)
 {
 	char	*line;
-	char	**split_line;
-	int		x;
-	int		y;
+	int		gnl;
 	int		fd;
+	t_list	*lst;
+	t_info	*info;
 
+	info = (t_info)ft_memalloc(sizeof(t_info));
+	info->width = -1;
+	info->z_min = MAX_INT;
+	info->z_max = MIN_INT;
+	lst = NULL;
 	fd = open_file(file_name);
-	y = 0;
-	while (get_next_line(fd, line) == 1)
+	while ((gnl = get_next_line(fd, &line)) == 1)
 	{
-		split_line = ft_strsplit(*line, ' ');
-		x = 0;
-		while (*split_line)
-		{
-			ft_strmap(*split_line, ft_atoi);
-			split_line++;
-		}
+		if (info->width == -1)
+			info->width = ft_count_words(line, ' ');
+		if (info->width != ft_count_words(line, ' '))
+			ft_putstr("The map is invalid!");
+		ft
 	}
 }
